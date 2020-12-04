b7c122eefba8335507b72bdd0237934ba60aed5c
pedromesmer <pedro_mesmer@hotmail.com>
2020-12-04 08:41:07 -0300

    success

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    presets: [
+        // env -> envoiment -> ambiente
+        '@babel/preset-env', // converte o código do JS mais antigo para um mais moderno, baseano no ambiente de desenvolvimento
+        '@babel/preset-react' // adiciona as funcionalidades do react a conversão
+    ]
+}
